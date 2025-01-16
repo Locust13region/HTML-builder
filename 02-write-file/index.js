6b0479411b29d6e75f5d83c73d9e939a67bfc3ff
DESKTOP-MF8V5D1\Andrey <m>
2025-01-16 13:55:08 +0300

    02 write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const outputPath = path.resolve(__dirname, 'text.txt');
+const stream = fs.createWriteStream(outputPath, { flags: 'a' });
+
+const rl = readline.createInterface({ input, output });
+
+console.log('Type some text here or "exit" to quit.');
+
+rl.on('line', (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    console.log('exit');
+    rl.close();
+  } else {
+    stream.write(input + '\n');
+  }
+});
+
+rl.on('close', () => {
+  stream.end();
+  process.exit(0);
+});
+
+process.on('SIGINT', () => {
+  console.log('\nCtrl+C interruption');
+  rl.close();
+});
