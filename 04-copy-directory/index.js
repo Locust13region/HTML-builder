f67ef2e701d123810a7660b3bb259e72215b5bc6
DESKTOP-MF8V5D1\Andrey <m>
2025-01-16 20:29:48 +0300

    feat: 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,26 @@
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
+const { join } = require('path');
+const sourcePath = join(__dirname, 'files');
+const targetPath = join(__dirname, 'files-copy');
+
+async function copyDir(sourcePath, targetPath) {
+  try {
+    await rm(targetPath, { recursive: true, force: true });
+    await mkdir(targetPath);
+    const files = await readdir(sourcePath, { withFileTypes: true });
+
+    for (const file of files) {
+      const srcFilePath = join(sourcePath, file.name);
+      const trgFilePath = join(targetPath, file.name);
+      if (file.isFile) {
+        await copyFile(srcFilePath, trgFilePath);
+      } else if (file.isDirectory) {
+        copyDir(srcFilePath, trgFilePath);
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+copyDir(sourcePath, targetPath);
